389c0f42f42a8584727a0214ee2e4b6864cbebaa
leiderCM <142425158+leiderCM@users.noreply.github.com>
2023-10-11 15:04:06 -0500

    Update EmpleadoPorHoras.java

--- src/parcial/poo/estructura/EmpleadoPorHoras.java
+++ src/parcial/poo/estructura/EmpleadoPorHoras.java
@@ -1,28 +1,20 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package parcial.poo.estructura;
 
-/**
- *
- * @author Leider Ceron
- */
+
 public class EmpleadoPorHoras extends Empleado {
     private int horasTrabajadas;
     private double tarifasPorHoras;
-    private String nombre;
-    //costructor vacio
-    public EmpleadoPorHoras(){
-        horasTrabajadas=0;
-        tarifasPorHoras=0;
-    }
+  
+    
     //costructor con parametros
-    public EmpleadoPorHoras(String mombre,int horasTrabajadas, double tarifasPorHoras) {
+    public EmpleadoPorHoras(String nombre ,int horasTrabajadas, double tarifasPorHoras) {
+        super(nombre);
         this.horasTrabajadas = horasTrabajadas;
         this.tarifasPorHoras = tarifasPorHoras;
-        this.nombre=nombre;
+        
     }
+
     public int getHorasTrabajadas() {
         return horasTrabajadas;
     }
@@ -38,14 +30,6 @@ public class EmpleadoPorHoras extends Empleado {
     public void setTarifasPorHoras(double tarifasPorHoras) {
         this.tarifasPorHoras = tarifasPorHoras;
     }
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-    
     @Override
     public  double calaularSalarioEmpleado(){
         double salario;
@@ -53,12 +37,9 @@ public class EmpleadoPorHoras extends Empleado {
         
         return salario;
     }
-    
-   @Override
+
+    @Override
     public String toString() {
         return "\nnombre: "+nombre+"\nHoras trabajadas: "+horasTrabajadas+"\ntarifas por horas:"+tarifasPorHoras;
     }
-      
-    
-    
 }
